9d6f9dd7dcff019f6cbad3c3440d1349501efcca
josuastg <josuastg93@gmail.com>
2020-03-19 19:52:21 +0700

    adding component text donwload

---
+++ src/components/home/TextDownload.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div class="style-download">
+      <p class="download">Download Our Apps</p>
+      <p class="sub-download">Send packages through your phone and track your shipment</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.download {
+  font-size: 24px;
+  font-weight: 600;
+  text-align: left;
+  font-style: normal;
+  color: #d11f40;
+}
+.sub-download {
+  object-fit: contain;
+  font-size: 14px;
+  width: 300px;
+  text-align: left;
+  height: 24px;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+}
+.style-download {
+  display: flex;
+  flex-direction: column;
+}
+</style>
