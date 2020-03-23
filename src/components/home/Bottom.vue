5ceb799c2a3f09ada4f0b4a5f124017b5eae73ed
josuastg <josuastg93@gmail.com>
2020-03-23 09:38:09 +0700

    adding component bottom

---
+++ src/components/home/Bottom.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="bottom">
+    <p class="text">@ 2020 All Rights Reserved- Lion Parcel</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.bottom {
+  width: 100%;
+  display: flex;
+  position: absolute;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 200px;
+  height: 40px;
+  background-image: linear-gradient(to top, #f3f3f3, #ffffff);
+}
+.text {
+  font-weight: bold;
+  object-fit: contain;
+  font-size: 13px;
+  font-stretch: normal;
+  font-style: normal;
+  text-align: center;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #1a1421;
+}
+</style>
