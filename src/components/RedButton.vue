fe89ac1bab4c8ad0d018aca7d6e057071d82cbf4
josuastg <josuastg93@gmail.com>
2020-03-23 16:45:15 +0700

    change font family

--- src/components/RedButton.vue
+++ src/components/RedButton.vue
@@ -1,20 +1,22 @@
 <template>
-    <button class="button is-danger is-hovered">
-      <p class="title">{{title}}</p>
-    </button>
+  <a >
+    <button class="button is-danger is-hovered"> <p class="title"> {{title}}</p></button>
+  </a>
 </template>
 
 <script>
 export default {
-  props: ["title"]
+  props: ["title", "link"]
 };
 </script>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css?family=Poppins&display=swap");
 .title {
   font-weight: 600;
   width: 82px;
   height: 19px;
+  font-family: "Poppins", sans-serif;
   font-stretch: normal;
   font-style: normal;
   font-size: 14px;
