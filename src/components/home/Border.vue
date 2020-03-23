89011d2b9f96d8c31414a24da06b498b1f4ebca5
josuastg <josuastg93@gmail.com>
2020-03-23 09:36:57 +0700

    adding component border

---
+++ src/components/home/Border.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- <div class="container"> -->
+    <div class="top-border" />
+  <!-- </div> -->
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+
+.top-border {
+  height: 0.5px;
+  width: 100%;
+  border-style: solid;
+  background-size: cover;
+  border-color: whitesmoke;
+  border-width: 0.5px;
+}
+</style>
