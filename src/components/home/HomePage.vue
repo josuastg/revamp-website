210ef61830c378ff53331a530ce42a9769a63280
josuastg <josuastg93@gmail.com>
2020-03-23 09:57:33 +0700

    change styling css

--- src/components/home/HomePage.vue
+++ src/components/home/HomePage.vue
@@ -8,14 +8,12 @@
     </section>
     <div class="container">
       <section class="section">
-        <div class="columns">
-          <div class="column">
+        <div class="level">
+          <div class="level-left">
             <app-mobile-logo></app-mobile-logo>
-          </div>
-          <div class="column">
             <app-text-download></app-text-download>
           </div>
-          <div class="column">
+          <div class="level-right">
             <app-track-card></app-track-card>
           </div>
         </div>
@@ -24,15 +22,46 @@
         <app-cloud-logo></app-cloud-logo>
       </section>
       <section class="section">
-        <div class="track-section">
-          <app-reason-card></app-reason-card>
+        <div class="level">
+          <div class="level-left">
+            <p class="help-title">Program</p>
+          </div>
+          <!-- <div class="level-right"> -->
+          <p class="sub">See All Promo</p>
+          <!-- </div> -->
         </div>
+
+        <carousel
+          :perPageCustom="[[360, 1], [1024, 3]]"
+          :autoplay="true"
+          :scrollPerPage="true"
+          paginationActiveColor="#d11f40"
+        >
+          <slide>
+            <app-carousel></app-carousel>
+          </slide>
+          <slide>
+            <app-carousel></app-carousel>
+          </slide>
+          <slide>
+            <app-carousel></app-carousel>
+          </slide>
+          <slide>
+            <app-carousel></app-carousel>
+          </slide>
+        </carousel>
       </section>
       <section class="section">
-        <div>
-          <app-footer></app-footer>
-        </div>
+        <p class="help-title">Why Us ?</p>
+        <app-reason-card></app-reason-card>
       </section>
+      <section class="section">
+        <app-border></app-border>
+      </section>
+      <section class="section">
+        <app-footer></app-footer>
+      </section>
+      <app-bottom></app-bottom>
     </div>
   </div>
 </template>
@@ -40,7 +69,11 @@
 <script>
 import Title from "./Title";
 import MobileLogo from "./MobileLogo";
+import { Carousel, Slide } from "vue-carousel";
+import PromoCarousel from "./PromoCarousel";
 import ShippingCard from "./ShippingCard";
+import Bottom from "./Bottom";
+import Border from "./Border";
 import TextDownload from "./TextDownload";
 import TrackCard from "./TrackCard";
 import ReasonCard from "./ReasonCard";
@@ -56,7 +89,12 @@ export default {
     "app-track-card": TrackCard,
     "app-cloud-logo": CloudLogo,
     "app-reason-card": ReasonCard,
-    "app-footer": Footer
+    "app-footer": Footer,
+    "app-carousel": PromoCarousel,
+    "app-border": Border,
+    "app-bottom": Bottom,
+    carousel: Carousel,
+    slide: Slide
   },
   data() {
     return {
@@ -92,7 +130,27 @@ export default {
   background-image: linear-gradient(to bottom, rgb(255, 0, 0, 0), #ffffff);
 }
 
-.track-section {
-  margin-top: 80px;
+.help-title {
+  object-fit: contain;
+  font-size: 18px;
+  text-align: left;
+  margin-left: 20px;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+}
+.sub {
+  object-fit: contain;
+  font-size: 13px;
+  font-weight: 500;
+  font-stretch: normal;
+  margin-left: 20px;
+  margin-right: 20px;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #d11f40;
 }
 </style>
