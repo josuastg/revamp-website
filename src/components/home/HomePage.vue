c92e8691940225e1ffe58b347986116644a91329
josuastg <josuastg93@gmail.com>
2020-03-24 21:41:13 +0700

    fixing display in mobile

--- src/components/home/HomePage.vue
+++ src/components/home/HomePage.vue
@@ -1,83 +1,86 @@
 <template>
   <div>
     <section class="section">
-      <div :style="item" class="bg-home-second">
-        <app-title>
-          <div class="button-group">
-            <div class="columns">
-              <div class="column">
-                <app-check-price-button title="Cek Tarif" href="#shipping-card"></app-check-price-button>
+      <div :style="item" class="bg-home-second"></div>
+    </section>
+    <app-title>
+      <div class="button-group">
+        <div class="columns">
+          <div class="column">
+            <app-check-price-button title="Cek Tarif" href="#shipping-card"></app-check-price-button>
+          </div>
+          <div class="column">
+            <app-track-button title="Lacak" href="#track-card"></app-track-button>
+          </div>
+        </div>
+      </div>
+    </app-title>
+    <div class="second-content">
+      <section class="section">
+        <div class="container">
+          <section class="section" id="shipping-card">
+            <app-shipping-card></app-shipping-card>
+          </section>
+          <section class="section" id="track-card">
+            <div class="level">
+              <div class="level-left">
+                <app-mobile-logo v-if="isDesktop()"></app-mobile-logo>
+                <app-text-download v-if="isDesktop()"></app-text-download>
               </div>
-              <div class="column">
-                <app-track-button title="Lacak" href="#track-card"></app-track-button>
+              <div class="level-right">
+                <app-track-card>
+                  <app-mobile-logo v-if="isMobile()"></app-mobile-logo>
+                  <app-text-download v-if="isMobile()"></app-text-download>
+                </app-track-card>
               </div>
             </div>
-            <!-- <div class="buttons"> -->
-            <!-- </div> -->
-          </div>
-        </app-title>
+          </section>
+          <section class="section">
+            <app-cloud-logo></app-cloud-logo>
+          </section>
+          <section class="section">
+            <div class="level">
+              <div class="level-left">
+                <p id="help-title">Promo</p>
+              </div>
+              <div class="level-right">
+                <p class="sub">Lihat Semua</p>
+              </div>
+            </div>
+            <carousel
+              :perPageCustom="[[360, 1], [1024, 3]]"
+              :autoplay="true"
+              :scrollPerPage="true"
+              paginationActiveColor="#d11f40"
+            >
+              <slide>
+                <app-carousel></app-carousel>
+              </slide>
+              <slide>
+                <app-carousel></app-carousel>
+              </slide>
+              <slide>
+                <app-carousel></app-carousel>
+              </slide>
+              <slide>
+                <app-carousel></app-carousel>
+              </slide>
+            </carousel>
+          </section>
+          <section class="section">
+            <p id="help-title">Mengapa Kami ?</p>
+            <app-reason-card></app-reason-card>
+          </section>
+          <section class="section">
+            <app-border></app-border>
+            <section class="section">
+              <app-footer></app-footer>
+            </section>
+          </section>
 
-        <section class="section" id="shipping-card">
-          <app-shipping-card></app-shipping-card>
-        </section>
-      </div>
-    </section>
-    <div class="container">
-      <section class="section" id="track-card">
-        <div class="level">
-          <div class="level-left">
-            <app-mobile-logo></app-mobile-logo>
-            <app-text-download></app-text-download>
-          </div>
-          <div class="level-right">
-            <app-track-card></app-track-card>
-          </div>
+          <app-bottom></app-bottom>
         </div>
       </section>
-      <section class="section">
-        <app-cloud-logo></app-cloud-logo>
-      </section>
-      <section class="section">
-        <div class="level">
-          <div class="level-left">
-            <p id="help-title">Promo</p>
-          </div>
-          <div class="level-right">
-            <p class="sub">Lihat Semua</p>
-          </div>
-        </div>
-        <carousel
-          :perPageCustom="[[360, 1], [1024, 3]]"
-          :autoplay="true"
-          :scrollPerPage="true"
-          paginationActiveColor="#d11f40"
-        >
-          <slide>
-            <app-carousel></app-carousel>
-          </slide>
-          <slide>
-            <app-carousel></app-carousel>
-          </slide>
-          <slide>
-            <app-carousel></app-carousel>
-          </slide>
-          <slide>
-            <app-carousel></app-carousel>
-          </slide>
-        </carousel>
-      </section>
-      <section class="section">
-        <p id="help-title">Mengapa Kami ?</p>
-        <app-reason-card></app-reason-card>
-      </section>
-      <section class="section">
-        <app-border></app-border>
-        <section class="section">
-          <app-footer></app-footer>
-        </section>
-      </section>
-
-      <app-bottom></app-bottom>
     </div>
   </div>
 </template>
@@ -122,24 +125,71 @@ export default {
         backgroundImage: `url(${require("../../assets/bg_crop_second.png")})`
       }
     };
+  },
+  created() {
+    window.addEventListener("resize", this.isMobile);
+    this.isMobile();
+    this.isDesktop();
+  },
+  destroyed() {
+    window.addEventListener("resize", this.isMobile);
+  },
+  methods: {
+    isMobile() {
+      if (
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent
+        )
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    isDesktop() {
+      if (
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent
+        )
+      ) {
+        return false;
+      } else {
+        return true;
+      }
+    }
   }
 };
 </script>
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css?family=Poppins&display=swap");
-.bg-home-second {
-  background-size: cover;
-  background-position: center;
-  object-fit: contain;
-  justify-content: auto;
-  display: flex;
-  flex-direction: column;
-  height: 714.5px;
-  width: 100%;
-  background-repeat: no-repeat;
-  position: relative;
-  height: 800px;
+@media (min-width: 360px) {
+  .bg-home-second {
+    background-size: cover;
+    object-fit: contain;
+    height: 200px;
+    background-position: center;
+    position: relative;
+    margin-top: 20px;
+    width: 100%;
+    background-repeat: no-repeat;
+    position: relative;
+  }
+}
+@media (min-width: 600px) {
+  .bg-home-second {
+    background-size: cover;
+    background-position: center;
+    object-fit: contain;
+    justify-content: auto;
+    display: flex;
+    flex-direction: column;
+    height: 714.5px;
+    width: 100%;
+    background-repeat: no-repeat;
+    position: absolute;
+    height: 800px;
+  }
 }
 .bg-home-second:before {
   content: "";
@@ -182,5 +232,10 @@ export default {
   display: flex;
   justify-content: center;
   flex-direction: row;
+}
+@media (min-width: 600px) {
+  .second-content {
+    margin-top: 340px;
+  }
 }
 </style>
