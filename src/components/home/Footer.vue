5ff89c9aeda64350c9db09a8b43bf61efb88d813
josuastg <josuastg93@gmail.com>
2020-03-24 21:40:30 +0700

    fixing layout

--- src/components/home/Footer.vue
+++ src/components/home/Footer.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>
-    <div class="content">
+  <div class="content">
+    <div class="footer-content">
       <div class="columns">
         <div class="column">
-          <p class="title">Lion Parcel</p>
+          <div class="content-on-mobile">
+            <p class="title">Lion Parcel</p>
+            <div v-if="isMobile()">
+              <p class="title">Ikuti Kami</p>
+              <div class="social">
+                <img src="../../assets/twitter.png" class="social-media" />
+                <img src="../../assets/fb.png" class="social-media" />
+                <img src="../../assets/instagram.png" class="social-media" />
+              </div>
+            </div>
+          </div>
           <div class="content-subtitle">
+            <br />
             <p class="subtitle">Tentang Kami</p>
             <p class="subtitle">Faq</p>
             <p class="subtitle">Karir</p>
@@ -39,7 +50,6 @@
                 </p>
               </div>
             </div>
-
             <div class="content-icon">
               <img src="../../assets/mail.png" class="icons" />
               <p class="subtitle">
@@ -50,8 +60,9 @@
           </div>
         </div>
         <div class="column">
+          <br />
+          <p class="title"></p>
           <div class="content-icon">
-            <br />
             <img src="../../assets/clock.png" class="icons" />
             <p class="subtitle">
               Monday to Sunday
@@ -59,7 +70,7 @@
             </p>
           </div>
         </div>
-        <div class="column">
+        <div class="column" v-if="isDesktop()">
           <p class="title">Ikuti Kami</p>
           <div class="social">
             <img src="../../assets/twitter.png" class="social-media" />
@@ -80,37 +91,143 @@
 </template>
 
 <script>
-export default {};
+export default {
+  created() {
+    window.addEventListener("resize", this.isMobile, this.isDesktop);
+    this.isMobile();
+    this.isDesktop();
+  },
+  destroyed() {
+    window.addEventListener("resize", this.isMobile, this.isDesktop);
+  },
+  methods: {
+    isMobile() {
+      if (
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent
+        )
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    isDesktop() {
+      if (
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent
+        )
+      ) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+};
 </script>
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css?family=Poppins&display=swap");
 
+@media (min-width: 360px) {
+  .footer-content {
+    display: flex;
+    justify-content: center;
+  }
+}
+
+@media (min-width: 600px) {
+  .footer-content {
+    justify-content: center;
+    display: flex;
+  }
+}
+@media (min-width: 360px) {
+  .img-playstore {
+    width: 140px;
+    height: 39px;
+    object-fit: contain;
+  }
+}
+
+@media (min-width: 600px) {
+  .img-playstore {
+    width: 140px;
+    height: 40px;
+    object-fit: contain;
+  }
+}
+
+@media (min-width: 360px) {
+  .img-appstore {
+    width: 120px;
+    height: 40px;
+    object-fit: contain;
+  }
+}
+
+@media (min-width: 600px) {
+  .img-appstore {
+    width: 120px;
+    height: 40px;
+    object-fit: contain;
+  }
+}
+
+@media (min-width: 360px) {
+  .img-content {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+}
+
+@media (min-width: 600px) {
+  .img-content {
+    display: flex;
+    flex-direction: row;
+  }
+}
+
+@media (min-width: 360px) {
+  .getapp {
+    font-weight: bold;
+    font-size: 18px;
+    object-fit: contain;
+    font-family: "Poppins", sans-serif;
+    font-stretch: normal;
+    font-style: normal;
+    position: relative;
+    line-height: normal;
+    text-align: left;
+    letter-spacing: normal;
+  }
+}
+@media (min-width: 600px) {
+  .getapp {
+    font-weight: bold;
+    font-size: 13.8px;
+    object-fit: contain;
+    font-family: "Poppins", sans-serif;
+    font-stretch: normal;
+    display: flex;
+    justify-content: flex-end;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+  }
+}
 .content-subtitle {
   display: flex;
   flex-direction: column;
   line-height: normal;
   letter-spacing: normal;
 }
-.img-playstore {
-  width: 140px;
-  height: 40px;
-  object-fit: contain;
-}
-.img-appstore {
-  width: 120px;
-  height: 40px;
-  object-fit: contain;
-}
-.img-content {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-}
 
 .title {
   font-weight: bold;
-  font-size: 16px;
+  font-size: 14px;
   object-fit: contain;
   font-family: "Poppins", sans-serif;
   font-stretch: normal;
@@ -123,22 +240,16 @@ export default {};
   font-size: 12px;
   object-fit: contain;
   padding-left: 10px;
-  text-align: justify;
+  text-align: left;
   font-family: "Poppins", sans-serif;
   font-stretch: normal;
   font-style: normal;
 }
-.getapp {
-  font-weight: bold;
-  font-size: 18px;
-  object-fit: contain;
-  font-family: "Poppins", sans-serif;
-  font-stretch: normal;
-  text-align: right;
-  text-align: center;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: normal;
+
+.content-on-mobile {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
 .social-media {
   flex-direction: row;
@@ -164,7 +275,7 @@ export default {};
   flex-direction: row;
   margin: 7px;
 }
-.customer-service{
+.customer-service {
   padding-left: 14px;
 }
 </style>
