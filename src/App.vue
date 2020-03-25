f129685bd4ca9c62ecd3b6010b31198909b144e0
josuastg <josuastg93@gmail.com>
2020-03-26 00:02:46 +0700

    fixing stucture div

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <div > 
- <app-nav-bar></app-nav-bar>
-  <router-view></router-view>
+  <div>
+    <app-nav-bar></app-nav-bar>
+    <div >
+      <router-view></router-view>
+    </div>
   </div>
- 
 </template>
 
 <script>
