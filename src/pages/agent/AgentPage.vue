6a5f1e5e7a2ffcb2f551d8a1b297eb73df1cb686
josuastg <josuastg93@gmail.com>
2020-03-30 13:32:24 +0700

    adding page agent

---
+++ src/pages/agent/AgentPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 >This is agent </h1>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: "agent-page",
+  data() {
+    return {
+     
+    };
+  },
+  
+};
+</script>
+
+<style scoped>
+</style>
