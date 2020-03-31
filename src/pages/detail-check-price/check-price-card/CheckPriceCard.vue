0c4e5ff0ebb1ac0afcdd980b4604f88887984829
josuastg <josuastg93@gmail.com>
2020-03-31 21:10:55 +0700

    fixing layout check price card

---
+++ src/pages/detail-check-price/check-price-card/CheckPriceCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="shipping-content-detail">
+    <div class="container">
+      <div class="card">
+        <div class="card-content">
+          <p class="check-tarif">Cek Tarif</p>
+          <div class="columns">
+            <div class="column">
+              <p class="ship-title-detail">Kota Asal</p>
+              <input class="input" type="text" placeholder="Masukan alamat kota asal" />
+            </div>
+            <div class="column">
+              <p class="ship-title-detail">Kota Tujuan</p>
+              <input class="input" type="text" placeholder="Masukan alamat kota tujuan" />
+            </div>
+            <div class="column">
+              <p class="ship-title">Berat Paket</p>
+              <input
+                class="input"
+                type="number"
+                placeholder="Masukan berat paket (max.100kg)"
+                min="1"
+                max="100"
+              />
+            </div>
+            <div class="level">
+              <div class="btn">
+                <app-red-button title="Cek Tarif"></app-red-button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import RedButton from "../../../components/button/RedButton";
+
+export default {
+  components: {
+    "app-red-button": RedButton
+  }
+};
+</script>
+
+<style scoped>
+@import "./checkpricecard.css";
+</style>
