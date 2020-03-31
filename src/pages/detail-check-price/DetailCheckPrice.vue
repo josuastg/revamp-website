0b5d4734cff21ea64e46290b137c5ef771a7ecb6
josuastg <josuastg93@gmail.com>
2020-03-31 21:09:32 +0700

    adding page  detail check price

--- src/pages/detail-check-price/DetailCheckPrice.vue
+++ src/pages/detail-check-price/DetailCheckPrice.vue
@@ -1,19 +1,92 @@
 <template>
   <div>
     <section class="section">
-      <div class="bg-detail-check-price" :style="item" v-if="isDesktop()"></div>
+      <div class="bg-detail-check-price" :style="item" v-if="isDesktop()">
+        <div class="content-breadcrumb">
+          <nav class="breadcrumb" aria-label="breadcrumbs">
+            <ul>
+              <li>
+                <a href="/">
+                  <img src="../../assets/home.png" class="icon-home" />
+                </a>
+              </li>
+              <li class="is-active">
+                <a href="/tariff" aria-current="page" class="route-breadcrumb">Tarif</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
       <div class="bg-detail-check-price-mobile" :style="secondItem" v-if="isMobile()"></div>
     </section>
     <app-check-price-card></app-check-price-card>
+    <section class="section">
+      <div class="content-pack-card">
+        <div class="columns">
+          <div class="column">
+            <div class="pack-card" :style="regpack">
+              <div class="bubble-content">
+                <img src="../../assets/bubble_question.png" class="bubble-icon" />
+                <span class="tooltiptext">Pengiriman reguler</span>
+              </div>
+              <img src="../../assets/icon_regpack.png" class="icon-pack" />
+              <p class="title-pack">Rp. 11.000</p>
+              <p class="subtitle-pack">3 - 4 hari kerja</p>
+            </div>
+          </div>
+          <div class="column">
+            <div class="pack-card" :style="onepack">
+              <div class="bubble-content">
+                <img src="../../assets/bubble_question.png" class="bubble-icon" />
+                <span class="tooltiptext">Pengiriman 1 x 24 jam</span>
+              </div>
+              <img src="../../assets/icon_onepack.png" class="icon-pack" />
+              <p class="title-pack">Rp. 20.000</p>
+              <p class="subtitle-pack">1 hari sampai</p>
+            </div>
+          </div>
+          <div class="column">
+            <div class="pack-card" :style="landpack">
+              <div class="bubble-content">
+                <img src="../../assets/bubble_question.png" class="bubble-icon" />
+                <span class="tooltiptext">Pengiriman pake kereta</span>
+              </div>
+              <img src="../../assets/icon_landpack.png" class="icon-pack" />
+              <p class="title-pack">Rp. 10.000</p>
+              <p class="subtitle-pack">3 - 4 hari kerja</p>
+            </div>
+          </div>
+          <div class="column">
+            <div class="pack-card" :style="interpack">
+              <div class="bubble-content">
+                <img src="../../assets/bubble_question.png" class="bubble-icon" />
+                <span class="tooltiptext">Pengiriman luar negeri</span>
+              </div>
+              <img src="../../assets/icon_interpack.png" class="icon-pack" />
+              <p class="title-pack">Rp. 84.000</p>
+              <p class="subtitle-pack">1 Minggu</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <app-border></app-border>
+    <section class="section">
+      <app-footer></app-footer>
+    </section>
   </div>
 </template>
 
 <script>
-import CheckPriceCard from "../home/check-price-card/CheckPriceCard";
+import CheckPriceCard from "./check-price-card/CheckPriceCard";
+import Border from "../../components/border/Border";
+import Footer from "../../components/footer/Footer";
 export default {
   name: "detail-shipment-page",
   components: {
-    "app-check-price-card": CheckPriceCard
+    "app-check-price-card": CheckPriceCard,
+    "app-border": Border,
+    "app-footer": Footer
   },
   data() {
     return {
@@ -22,6 +95,18 @@ export default {
       },
       secondItem: {
         backgroundImage: `url(${require("../../assets/bg_detail_check_price_mobile.png")})`
+      },
+      regpack: {
+        backgroundImage: `url(${require("../../assets/regpack.png")})`
+      },
+      onepack: {
+        backgroundImage: `url(${require("../../assets/onepack.png")})`
+      },
+      landpack: {
+        backgroundImage: `url(${require("../../assets/landpack.png")})`
+      },
+      interpack: {
+        backgroundImage: `url(${require("../../assets/interpack.png")})`
       }
     };
   },
