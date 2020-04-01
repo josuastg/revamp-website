aec9117324c493fb6c33cee793e66ed7d4e739f0
josuastg <josuastg93@gmail.com>
2020-04-01 23:00:06 +0700

    adding new component claim card

---
+++ src/pages/detail-claim/claim-card/ClaimCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="shipping-content-detail">
+    <div class="container">
+      <div class="card">
+        <div class="card-content">
+          <p class="check-tarif">Klaim Paket</p>
+          <div class="columns">
+            <div class="column">
+              <p class="ship-title-detail">Input your claim number</p>
+              <input class="input" type="text" placeholder="Masukan nomor claim" />
+              <p class="claim-information">*Nomor klaim didapat dari saat pengajuan klaim / sms notifikasi dari Lion Parcel. </p>
+            </div>
+            <div class="level">
+              <div class="btn-track">
+                <app-red-button title="Kirim"></app-red-button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import RedButton from "../../../components/button/RedButton";
+
+export default {
+  components: {
+    "app-red-button": RedButton,
+  }
+};
+</script>
+
+<style scoped>
+@import "./claimcard.css";
+</style>
