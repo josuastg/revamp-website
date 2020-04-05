b25d692d697e7ad216f459900b50118f26eaede9
josuastg <josuastg93@gmail.com>
2020-04-05 14:52:43 +0700

    adding new pages new claim and style

---
+++ src/pages/new-claim/NewClaimPage.vue
@@ -0,0 +1,126 @@
+<template>
+  <div>
+    <section class="section-content" v-show="isDesktop()">
+      <div class="container">
+        <div class="content-breadcrumb-claim">
+          <nav class="breadcrumb" aria-label="breadcrumbs">
+            <ul>
+              <li>
+                <a href="/">
+                  <img src="../../assets/home.png" class="icon-home" />
+                </a>
+              </li>
+              <li class="route-breadcrumb">
+                <a href="/claim/259317" aria-current="page" class="is-active">Claim</a>
+              </li>
+              <li class="is-active">
+                <a href="/claim" aria-current="page" class="route-breadcrumb">Create a New Claim</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </section>
+    <p class="title-form-claim" v-if="isDesktop()">Klaim Form</p>
+    <section class="section-content" v-if="isMobile()">
+      <div :style="bgNewClaimMobile" class="bg-detail-new-claim-mobile"></div>
+    </section>
+    <p class="title-form-claim" v-if="isMobile()">Klaim Form</p>
+
+    <div class="new-claim-content">
+      <div class="content-leaf">
+        <!-- <img src="../../assets/left_leaf.png" class="green-leaf" /> -->
+        <div class="green-leaf" :style="greenLeaf">
+          <div class="purple-leaf" :style="purpleLeaf"></div>
+        </div>
+        <div class="ellipse" :style="ellipse" />
+      </div>
+      <app-claim-form></app-claim-form>
+
+      <div class="second-content-leaf">
+        <div class="red-leaf-mobile" :style="redLeaf" v-if="isMobile()">
+          <div class="second-red-leaf-mobile" :style="secondRedLeaf" v-if="isMobile()"></div>
+        </div>
+        <img src="../../assets/right_leaf.png" class="red-leaf-desktop" v-if="isDesktop()" />
+      </div>
+    </div>
+    <app-border></app-border>
+    <section class="section">
+      <app-footer></app-footer>
+    </section>
+    <app-bottom></app-bottom>
+  </div>
+</template>
+
+<script>
+import Border from "../../components/border/Border";
+import Footer from "../../components/footer/Footer";
+import Bottom from "../../components/bottom/Bottom";
+import ClaimForm from "./claim-form/ClaimForm";
+export default {
+  components: {
+    "app-border": Border,
+    "app-footer": Footer,
+    "app-bottom": Bottom,
+    "app-claim-form": ClaimForm
+  },
+  data() {
+    return {
+      greenLeaf: {
+        backgroundImage: `url(${require("../../assets/green_leaf.png")})`
+      },
+      purpleLeaf: {
+        backgroundImage: `url(${require("../../assets/purple_leaf.png")})`
+      },
+      ellipse: {
+        backgroundImage: `url(${require("../../assets/ellipse.png")})`
+      },
+      redLeaf: {
+        backgroundImage: `url(${require("../../assets/red_leaf.png")})`
+      },
+      secondRedLeaf: {
+        backgroundImage: `url(${require("../../assets/second_red_leaf.png")})`
+      },
+      bgNewClaimMobile: {
+        backgroundImage: `url(${require("../../assets/bg_mobile_new_claim.png")})`
+      }
+    };
+  },
+  created() {
+    window.addEventListener("resize", this.isMobile);
+    this.isMobile();
+    this.isDesktop();
+  },
+  destroyed() {
+    window.addEventListener("resize", this.isMobile);
+  },
+  methods: {
+    isMobile() {
+      if (
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent
+        )
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    isDesktop() {
+      if (
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent
+        )
+      ) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+@import "./newclaimpage.css";
+</style>
---
+++ src/pages/new-claim/claim-form/ClaimForm.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="container">
+    <div class="content-claim-form">
+      <div class="card">
+        <div class="inside-margin">
+          <div class="card-content">
+            <p class="ship-title-new-claim">ID Pengiriman</p>
+            <input class="input" type="text" placeholder="Contoh: 11-12-12345" />
+            <p class="ship-title-new-claim">Klaimer</p>
+            <div class="field">
+              <div class="control is-expanded">
+                <div class="select is-fullwidth">
+                  <select>
+                    <option>Pengirim</option>
+                    <option>Penerima</option>
+                    <option>Lainnya</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <p class="ship-title-new-claim">Nama Lengkap</p>
+            <input class="input" type="text" placeholder="Masukan nama lengkap." />
+            <div class="columns">
+              <div class="column">
+                <p class="title-file">Akun Bank</p>
+                <div class="upload-file" @click="$refs.file.click()" v-if="!image">
+                  <img src="../../../assets/uploads.png" class="icon-uploads" />
+                  <input type="file" ref="file" style="display: none" @change="onFileChange" />
+                  <p class="upload-text">Upload Photo</p>
+                </div>
+                <div class="content-image-load" v-else>
+                  <img :src="image" class="image-load" />
+                </div>
+                <p class="upload-text" @click="removeImage" v-show="cancelPhoto">Cancel</p>
+              </div>
+              <div class="column">
+                <p class="title-file">KTP</p>
+                <div class="upload-file" @click="$refs.file.click()" v-if="!ktp">
+                  <img src="../../../assets/uploads.png" class="icon-uploads" />
+                  <input type="file" ref="file" style="display: none" @change="onFileChangeKtp" />
+                  <p class="upload-text">Upload KTP</p>
+                </div>
+                <div class="content-image-load" v-else>
+                  <img :src="ktp" class="image-load" />
+                </div>
+                <p class="upload-text" @click="removeImageKtp" v-show="cancelKtp">Cancel</p>
+              </div>
+            </div>
+            <p class="ship-title-new-claim">Tipe Klaim</p>
+            <div class="field">
+              <div class="control is-expanded">
+                <div class="select is-fullwidth">
+                  <select>
+                    <option>MBG</option>
+                    <option>Kerusakan (Damaged)</option>
+                    <option>Kehilangan (Missing)</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <img src="../../../assets/example_captcha.png" class="example-captcha" />
+            <p class="ship-title-new-claim">Masukkan Simbol</p>
+            <div class="input-captcha">
+              <input class="input" type="text" />
+            </div>
+
+            <p class="claim-information-document">
+              Dengan menekan tombol diatas maka saya setuju untuk mengikuti syarat
+              dan ketentuan yang berlaku sesuai dengan
+              <a>Dokumen ini</a>
+            </p>
+            <div class="btn-submit" @click="navigateToSuccessClaim">
+              <div class="buttons are-medium">
+                <app-red-button title="Kirim"></app-red-button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+import RedButton from "../../../components/button/RedButton";
+export default {
+  data() {
+    return {
+      image: "",
+      ktp: "",
+      cancelPhoto: false,
+      cancelKtp: false
+    };
+  },
+  components: {
+    "app-red-button": RedButton
+  },
+  beforeMount() {
+    var vm = this;
+    console.log("before mounted");
+    this.removeImage();
+    this.removeImageKtp();
+    vm.get("img");
+  },
+  methods: {
+    navigateToSuccessClaim() {
+      this.$router.push("/success");
+    },
+    get(key) {
+      this.image = localStorage.getItem(key);
+      this.ktp = localStorage.getItem(key);
+    },
+    set(key) {
+      try {
+        localStorage.setItem(key, this.image);
+        localStorage.setItem(key, this.ktp);
+      } catch (e) {
+        alert(`Storage failed: ${e}`);
+      }
+    },
+    onFileChange(e) {
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length) {
+        return;
+      }
+      this.cancelPhoto = true;
+      this.createImage(files[0]);
+    },
+    onFileChangeKtp(e) {
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length) {
+        return;
+      }
+      this.cancelKtp = true;
+      this.createImageKtp(files[0]);
+    },
+    createImage(file) {
+      var reader = new FileReader();
+      var vm = this;
+      reader.onload = e => {
+        this.image = e.target.result;
+        console.log("isi image", this.image);
+        vm.set("img");
+      };
+      reader.readAsDataURL(file);
+    },
+    createImageKtp(file) {
+      var reader = new FileReader();
+      var vm = this;
+      reader.onload = e => {
+        this.ktp = e.target.result;
+        console.log("isi image", this.ktp);
+        vm.set("img");
+      };
+      reader.readAsDataURL(file);
+    },
+    removeImage() {
+      this.image = "";
+      this.showing = false;
+      localStorage.removeItem("img");
+    },
+    removeImageKtp() {
+      this.ktp = "";
+      this.cancelKtp = false;
+      localStorage.removeItem("img");
+    }
+  }
+};
+</script>
+
+<style scoped>
+@import "./claimform.css";
+</style>
